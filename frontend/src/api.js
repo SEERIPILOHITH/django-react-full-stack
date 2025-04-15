adeaf5b0dcaeb2db3ce5b4ba5ca7bb475b01c5d8
SEERIPILOHITH <lohithseeripi@gmail.com>
2025-04-15 10:00:21 +0530

    add backend atempt 2

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { ACCESS_TOKEN } from './constants'
 const apiurl="/choreo-apis/django-react/backendd/v1"
 const api =axios.create({
-    baseURL: import.meta.env.VITE_API_URL?import.meta.env.VITE_API_URL:apiurl,
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiurl
 });
 api.interceptors.request.use(
     (config) =>{
